50002ffdfe73f79f5d844224cf8b87270e84f9ed
Brenner Spear <blspear@gmail.com>
2021-12-09 15:57:45 -0600

    update contract metadata, and logo img

--- pages/api/v1/contract-metadata.ts
+++ pages/api/v1/contract-metadata.ts
@@ -10,7 +10,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         image: `https://${WEBSITE_URL}/logo.png`,
         external_link: `https://${WEBSITE_URL}`,
         seller_fee_basis_points: 800, // 8%,
-        fee_recipient: '0x7d0414B0622f485D0368602E76F502CabEf57bf4',
+        fee_recipient: '0x902A37155438982884ca26A5DBccf73f5ae8194b',
     };
 
     res.send(metadata);
